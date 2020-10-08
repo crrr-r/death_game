bdd20332c0f844b17a88df66c218c6fe269e2600
crrr-r <1239905865@qq.com>
2020-10-08 08:36:31 +0800

    数组

---
+++ cmake-build-debug/CMakeFiles/death_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/death_game.dir/main.c.obj"
+  "death_game.exe"
+  "death_game.exe.manifest"
+  "death_game.pdb"
+  "libdeath_game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/death_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int i[5] = {1,2,3,4,5};
+    printf("%d",i[5]);
+    return 0;
+}
